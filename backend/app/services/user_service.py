9a4a2a301e8869aef9018f2762c008e279f6cff1
jchan6532 <justinchan057@gmail.com>
2025-11-14 02:38:53 -0500

    implemented documents model
    refactored roles enum
    
    introduced mime type and status enums for documents
    
    refactored user routes file to include check_user_exits() service logic
    
    added documents model and refactored user model to include document relationship
    
    moved health api lower
    
    added import for documents model
    
    added name and created_at and updated_at for user pydantic model
    
    removed old init migration
    
    swapped old init migraiton for a new init migration since user id is not UUID instead of int

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -3,11 +3,14 @@ from app.models.user import User
 from app.schemas.user_schema import UserCreate
 
 def create_user(db: Session, data: UserCreate) -> User:
-    u = User(email=data.email)
-    db.add(u)
+    user = User(email=data.email, name=data.name)
+    db.add(user)
     db.commit()
-    db.refresh(u)
-    return u
+    db.refresh(user)
+    return user
 
 def get_users(db: Session) -> list[User]:
     return db.query(User).order_by(User.id.desc()).all()
+
+def check_user_exists(db: Session, email: str) -> bool:
+    return db.query(User).filter(User.email == email).first() is not None
