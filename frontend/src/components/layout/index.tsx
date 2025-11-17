1f46ec28db216a6bd99f673c88b614d54a5a8bee
jchan6532 <justinchan057@gmail.com>
2025-11-17 10:46:19 -0500

    removed unecessary link import

--- frontend/src/components/layout/index.tsx
+++ frontend/src/components/layout/index.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Topbar from '@/components/layout/TopBar';
 import { useAuthHook } from '@/hooks/useAuthHook';
 import Sidebar from '@/components/layout/SideBar';
