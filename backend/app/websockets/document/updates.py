37c3f39638b85f8c4465434801f39af2dc60d80d
jchan6532 <justinchan057@gmail.com>
2025-12-04 21:54:17 -0500

    fix CORS issue for BE and make redis listener async
    incorporate FE URL env inside originas
    
    add await for redis subscribe

--- backend/app/websockets/document/updates.py
+++ backend/app/websockets/document/updates.py
@@ -1,32 +1,27 @@
 import asyncio
 import json
-from redis import Redis
+from redis.asyncio import Redis
 
 from app.core.config import get_settings
 from app.websockets.document.manager import manager
 
 settings = get_settings()
-redis = Redis.from_url(settings.REDIS_URL)
+redis = Redis.from_url(settings.REDIS_URL, decode_responses=True)
 
 async def redis_updates_listener():
     pubsub = redis.pubsub()
-    pubsub.subscribe("document_updates")
-
-    loop = asyncio.get_running_loop()
+    await pubsub.subscribe("document_updates")
 
     while True:
-        message = await loop.run_in_executor(
-            None, 
-            pubsub.get_message, 
-            True, 
-            1.0
+        message = await pubsub.get_message(
+            ignore_subscribe_messages=True, 
+            timeout=1.0
         )
+
         if not message:
             await asyncio.sleep(0.01)
             continue
 
         if message["type"] == "message":
-            data = json.loads(message["data"])
-            doc_id = data["doc_id"]
-            
+            data = json.loads(message["data"])            
             await manager.notify(message=data)
